279f3f298b6f2a5b184b685e3db5917f1111df47
Renee St Louis <renee.stlouis@outlook.com>
2025-05-14 01:08:07 -0400

    Adding old uncommitted changes

---
+++ src/components/ClaimLeague.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card color="white" title="Claim a League">
+    <v-card-text>
+      Would you like to be set up as an admin for your derby league?
+    </v-card-text>
+  </v-card>
+</template>
